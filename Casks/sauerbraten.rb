136d6ec7bbc7b1156bbdec37b6b246a917c3d49d
Michael Rosenberg <michael@mrosenberg.pub>
2021-01-02 23:43:26 -0500

    Fixed sauerbraten URL

--- Casks/sauerbraten.rb
+++ Casks/sauerbraten.rb
@@ -1,5 +1,5 @@
 cask "sauerbraten" do
-  version "2020.11.29,2020.12.20"
+  version "2020.11.29,2020.12.21"
   sha256 "47e61c7a3df2777735a22b8569d99030e0dbf5e3e2fd800380dd1a8f6fd63eb2"
 
   url "https://downloads.sourceforge.net/sauerbraten/sauerbraten/#{version.before_comma.dots_to_underscores}/sauerbraten_#{version.after_comma.dots_to_underscores}_macos.dmg",
